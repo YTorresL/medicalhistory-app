ff07d859ea4aef9ea261fa900aee58ba37173e84
Yalith Torres <yalithdev@gmail.com>
2024-02-11 15:33:31 -0400

    .env

--- build/static/chunks/webpack.js
+++ build/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "0f0b488f670d47d9"; }
+/******/ 		__webpack_require__.h = function() { return "ef5073524a6903d6"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
